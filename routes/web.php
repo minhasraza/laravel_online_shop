ac4ac31d1c521ebbdf2a8f01ee98ca070bbfc418
Minhas Raza <80245250+minhasraza@users.noreply.github.com>
2023-08-18 18:48:10 +0500

    Admin panel
    Admin panel and admin authentication done.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ Route::group(['prefix' => 'admin'],function(){
+
+    Route::group(['middleware' => 'admin.guest'], function(){
+        Route:: get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route:: post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+
+    });
+    
+    Route::group(['middleware' => 'admin.auth'], function(){
+        Route:: get('/home', [HomeController::class, 'index'])->name('admin.home');
+        Route:: get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+    });
+ });
