016c50370fcea0ddc601b425e512c1ea7a2f6351
Minhas Raza <80245250+minhasraza@users.noreply.github.com>
2023-09-02 14:29:22 +0500

    Admin Pannel Complete with products module.

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -7,15 +7,23 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductImage;
+use App\Models\SubCategory;
 use App\Models\TempImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
 use Image;
 
 class ProductController extends Controller
 {
-    public function index(){
-        return view('admin.products.list');
+    public function index(Request $request){
+        $products = Product::latest('id')->with('product_images');
+        if ($request->get('keyword') != "") {
+            $products = $products->where('title','like','%'.$request->keyword.'%');
+        }
+        $products = $products->paginate();
+        $data['products'] = $products;
+        return view('admin.products.list', $data);
     }
     public function create(){
         $data = [];
@@ -82,7 +90,7 @@ class ProductController extends Controller
 
                     // Large Image
                     $sourcePath = public_path().'/temp/'.$tempImageInfo->name;
-                    $destPath = public_path().'/uploads/product/large/'.$tempImageInfo->name;
+                    $destPath = public_path().'/uploads/product/large/'.$imageName;
                     $image = Image::make($sourcePath);
                     $image->resize(1400,null,function($constraint){
                         $constraint->aspectRatio();
@@ -90,12 +98,14 @@ class ProductController extends Controller
                     $image->save($destPath);
 
                     // Small Image
-                    $destPath = public_path().'/uploads/product/small/'.$tempImageInfo->name;
+                    $destPath = public_path().'/uploads/product/small/'.$imageName;
                     $image = Image::make($sourcePath);
                     $image->fit(300,300);
                     $image->save($destPath);
                 }
             }
+
+            $request->session()->flash('success', 'Product added successfully');
 
             return response()->json([
                 'status' => true,
@@ -108,5 +118,141 @@ class ProductController extends Controller
                 'error' => $validator->errors()
             ]);
         }
+    }
+
+    public function edit($id, Request $request){    
+        $product = Product::find($id);
+        $subCategories = SubCategory::where('category_id',$product->category_id)->get();
+        $data = [];
+        $data['subCategories'] =  $subCategories;
+        $data['product'] = $product;
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $brands = Brand::orderBy('name', 'ASC')->get();
+        $data['categories'] = $categories;
+        $data['brands'] = $brands;
+        return view('admin.products.edit',$data);
+    }
+
+    public function update($id,Request $request){
+        $product = Product::find($id);
+        if (empty($product)) {
+            $request->session()->flash('error', 'Product not found');
+            return response([
+                'status' => false,
+                'notFound' => true,
+            ]);
+        }
+        // $subCategories = SubCategory::where('category_id',$product->category_id)->get();
+        $rules = [
+            'title' => 'required',
+            'slug' => 'required|unique:products,slug,'.$product->id.',id',
+            'price' => 'required|numeric',
+            'sku'=> 'required|unique:products,sku,'.$product->id.',id',
+            'track_qty'=> 'required|in:Yes,No',
+            'category' => 'required',
+            'is_featured' => 'required|in:Yes,No',
+
+        ];
+        if (!empty($request->track_qty) && $request->track_qty == 'Yes') {
+            $rules['qty'] = 'required|numeric';
+        }
+        $validator = Validator::make($request->all(),$rules);
+
+        if ($validator->passes()) {
+            // $product = new Product;
+            $product->title = $request->title;
+            $product->slug = $request->slug;
+            $product->price = $request->price;
+            $product->compare_price = $request->compare_price;
+            $product->sku = $request->sku;
+            $product->track_qty = $request->track_qty;
+            $product->description = $request->description;
+            $product->barcode = $request->barcode;
+            $product->qty = $request->qty;
+            $product->status = $request->status;
+            $product->category_id = $request->category;
+            $product->sub_category_id = $request->sub_category;
+            $product->brand_id = $request->brand;
+            $product->is_featured = $request->is_featured;
+            $product->save();
+
+
+            // Save Gallery Pics
+
+            if (!empty($request->image_array)) {
+                foreach ($request->image_array as $temp_image_id) {
+
+                    $tempImageInfo = TempImage::find($temp_image_id);
+                    $extArray = explode('.',$tempImageInfo->name);
+                    $ext = last($extArray); // like jpg,gif,png etc
+                    $productImage = new ProductImage();
+                    $productImage->product_id = $product->id;
+                    $productImage->image = 'NULL';
+                    $productImage->save();
+
+                    $imageName = $product->id.'-'.$productImage->id.'-'.time().'.'.$ext;
+                    $productImage->image = $imageName;
+                    $productImage->save();
+
+                    // Generate Product Thumbnails
+
+                    // Large Image
+                    $sourcePath = public_path().'/temp/'.$tempImageInfo->name;
+                    $destPath = public_path().'/uploads/product/large/'.$imageName;
+                    $image = Image::make($sourcePath);
+                    $image->resize(1400,null,function($constraint){
+                        $constraint->aspectRatio();
+                    });
+                    $image->save($destPath);
+
+                    // Small Image
+                    $destPath = public_path().'/uploads/product/small/'.$imageName;
+                    $image = Image::make($sourcePath);
+                    $image->fit(300,300);
+                    $image->save($destPath);
+                }
+            }
+
+            $request->session()->flash('success', 'Product updated successfully');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Product updated successfully'
+            ]);
+            
+        }else{
+            return response()->json([
+                'status' => false,
+                'error' => $validator->errors()
+            ]);
+        }        
+    }
+
+    public function destroy($id, Request $request){
+        $product = Product::find($id);
+
+        if (empty($product)) {
+            $request->session()->flash('error', 'Product not found');
+            return response()->json([
+                'status' => false,
+                'notFound' => true
+            ]);
+        }
+
+        $productImages = ProductImage::where('product_id', $id)->get();
+        if (!empty($productImages)) {
+            foreach ($productImages as $productImage) {
+                File::delete(public_path('uploads/product/large/'.$productImage->image));
+                File::delete(public_path('uploads/product/small/'.$productImage->image));
+            }
+            ProductImage::where('product_id', $id)->delete();
+        }
+
+        $product->delete();
+        $request->session()->flash('success', 'Product deleted successfully');
+        return response()->json([
+            'status' => true,
+            'message' => 'Product deleted successfullt'
+        ]);
     }
 }
